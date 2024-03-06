a287e583a5e529fa06ccfcd5482ababa7ba87424
RisvanBCA2022 <111649025+RisvanBCA2022@users.noreply.github.com>
2024-03-06 13:34:59 +0530

    custom navbar added

---
+++ src/config/index.ts
@@ -0,0 +1,46 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label:"Processors",
+        value:"Processors" as const,
+        featured:[
+            {
+                name:"builder Picks",
+                href:"#",
+                imageSrc:'/nav/ui-kits/mixed.jpg'
+            },
+            {
+                name:"New Pre-Builts",
+                href:"#",
+                imageSrc:'/nav/ui-kits/blue.jpg'
+            },
+            {
+                name:"Best Buitls",
+                href:"#",
+                imageSrc:'/nav/ui-kits/purple.jpg'
+            },
+            
+        ]
+    },
+    {
+        label:"icons",
+        value:"icons" as const,
+        featured:[
+            {
+                name:"Favorite Icon Picks",
+                href:"#",
+                imageSrc:'/nav/icons/picks.jpg'
+            },
+            {
+                name:"New Pre-Builts",
+                href:"#",
+                imageSrc:'/nav/icons/new.jpg'
+            },
+            {
+                name:"Best Buitls",
+                href:"#",
+                imageSrc:'/nav/icons/bestsellers.jpg'
+            },
+            
+        ]
+    }
+]
